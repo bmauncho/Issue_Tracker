6482a02d035c5c5ae7519f39e83c61ce2a1f1786
BradleyMauncho <bmauncho@gmail.com>
2024-09-29 17:09:06 +0300

    Api for creating issues

--- app/api/Issues/route.ts
+++ app/api/Issues/route.ts
@@ -7,14 +7,14 @@ const createIssueSchema = z.object({
   description: z.string().min(1),
 });
 
-export async function Post(request: NextRequest) {
+export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  const newIssue = prisma.issue.create({
-    data: { title: body.tittle, description: body.description },
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
   });
   return NextResponse.json(newIssue, { status: 201 });
 }
